d539b138f9cc8037037dee8f0ff9532f9571b6ac
Mogkc <carson.mogk@gmail.com>
2015-02-12 23:57:39 -0800

    Changed Message Payload
    Only delivers one integer now

--- src/com/mrjaffesclass/apcs/mvc/template/MessagePayload.java
+++ src/com/mrjaffesclass/apcs/mvc/template/MessagePayload.java
@@ -9,33 +9,22 @@ package com.mrjaffesclass.apcs.mvc.template;
  */
 public class MessagePayload {
   
-  private final int field;
-  private final int direction;
+  private final int delivery;
   
   /**
    * Class constructor
    * @param _field Text field 1 or 2
    * @param _direction Direction (Constants.UP or Constants.DOWN)
    */
-  public MessagePayload(int _field, int _direction) {
-    field = _field;
-    direction = _direction;
+  public MessagePayload(int _delivery) {
+    delivery = _delivery;
   }
   
   /**
-   * Getter method for the direction
-   * @return Field value
+   * Getter method for the package delivery
+   * @return delivery value
    */
-  public int getDirection() {
-    return direction;
+  public int getDelivery() {
+    return delivery;
   }
-  
-  /**
-   * Getter method for the 
-   * @return 
-   */
-  public int getField() {
-    return field;
-  }
-  
 }
